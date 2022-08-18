395c3b01ac414cb18fbb2e850c08f4d7c841f349
Kevin Leister <kevinleister07@gmail.com>
2022-08-18 13:51:09 -0500

    create route chains for articles to handle requests

--- app.js
+++ app.js
@@ -28,12 +28,92 @@ const articleSchema = {
 };
 // Create model
 const Article = mongoose.model("Article", articleSchema);
+// Create get, post, and delete route chain for requests targeting all articles
+app.route("/articles")
+.get(function(req, res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+})
+.post(function(req, res){
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+})
+.delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully deleted all articles.");
+        } else {
+            res.send(err);
+        }
+    });
+    });
+    ///////////////////////// End all article route chain ////////////////////////////
+// Route chain for request targeting a specific article
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if(foundArticle){
+        res.send(foundArticle);
+    } else {
+        res.send("No articles matching that title was found.");
+    }
+});
+})
+.put(function(req, res){
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article.");
+            }
+        }
+    );
+})
+.patch(function(req, res){
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article.");
+            } else {
+                res.send(err);
+            }
+        }
+    );
+})
+.delete(function(req, res){
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err){
+            if(!err){
+                res.send("Successfully deleted the corresponding article.");
+            } else {
+                res.send(err);
+            }
+        }
+    );
+});
 
 
 
 
-
-    // End async function main
+///////////////////////////// End async function main ////////////////////////
 };
 
 
